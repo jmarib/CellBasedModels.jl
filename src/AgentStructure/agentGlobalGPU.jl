fc8a05ebdfa61a99d84d642a27acee4ee5e22dbe
gatocor <g.torregrosa@outlook.com>
2025-11-18 16:58:47 +0100

    DELETE old agent files

--- src/AgentStructure/agentGlobalGPU.jl
+++
@@ -1,5 +0,0 @@
-CellBasedModels.toCPU(cp::CommunityPoint{CommunityPointMeta}) = Adapt.adapt(SizedVector, cp)
-CellBasedModels.toCPU(cp::CommunityPoint{CommunityPointMeta{<:SizedVector, <:SizedVector}}) = cp
-
-CellBasedModels.toGPU(cp::CommunityPoint{CommunityPointMeta}) = cp
-CellBasedModels.toGPU(cp::CommunityPoint{B}) where {B<:CommunityPointMeta{<:SizedVector, <:SizedVector}} = Adapt.adapt(CuArray, cp)
